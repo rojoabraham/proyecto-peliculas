c65c4be5066498b04539ca38373708d01a944dfa
rojoabraham <rojo.abraham@gmail.com>
2024-02-19 23:55:15 -0700

    se agregó pantalla de about sencilla

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-7 col-sm-6">
+            <div className="about-title clearfix">
+              <h1 className="">
+                About Abraham
+              </h1>
+              <h3>Abraham <span style={{color: "red"}}>Rojo</span> </h3>
+              <p className="about-paddingB">
+                Proyecto de API de Películas con React
+              </p>
+              <p>
+                Aplicación que llama a la API de TMDB para traer películas. Usa Bootstrap y React básico.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
   )
 }
 
